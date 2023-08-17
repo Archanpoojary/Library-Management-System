d3f8f442ab3982331b9983a40bbc622548e15dbe
Archana Poojary <archanm411@gmail.com>
2023-08-17 14:17:54 +0530

    handleRemove function added

--- src/components/userList.jsx
+++ src/components/userList.jsx
@@ -13,6 +13,19 @@ const UserList = () => {
         fetchData()
     },[])
 
+    // let handleRemove=(name)=>{
+    //     fetch(`http://localhost:4000/users/${name}`,{
+    //         method:'DELETE'
+    //     },[]);
+    //     alert(`${name} will be deleted permanently`)
+    // }
+    let handleRemove=(id,name)=>{
+        setUser(user.filter(x=>x.id!=id))
+        alert(`${name} has been removed`)
+    }
+
+    
+
     return (
         <div className="usrlst">
             <h1>User List</h1>
@@ -23,6 +36,7 @@ const UserList = () => {
                         <h4>Age:{data.age}</h4>
                         <h4>Email:{data.email}</h4>
                         <h4>Phone Number:{data.phoneNumber}</h4>
+                        <button onClick={()=>handleRemove(data.id,data.name)}>Delete</button>
                     </div>
                 ))}
             </div>
